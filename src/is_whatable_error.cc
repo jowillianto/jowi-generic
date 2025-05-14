446c2d7aaa4a9cd5d04675315922eb27042c42cc
Jonathan Willianto <jowillianto@gmail.com>
2025-05-15 00:05:07 +0900

    [#main] remove make_error_message

--- src/is_whatable_error.cc
+++ src/is_whatable_error.cc
@@ -7,8 +7,4 @@ namespace moderna::generic {
   export template <class error_type>
   concept is_whatable_error =
     std::constructible_from<std::string_view, decltype(std::declval<error_type>().what())>;
-
-  export std::string_view make_error_message(const is_whatable_error auto &error) {
-    return error.what();
-  }
 }
