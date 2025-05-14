19781460aecd777d28b13978d81ed853a5472ad0
Jonathan Willianto <jowillianto@gmail.com>
2025-05-15 00:06:31 +0900

    [#main] stage error

---
+++ src/staged_error.cc
@@ -0,0 +1,47 @@
+module;
+#include <exception>
+#include <optional>
+#include <string>
+export module moderna.generic:staged_error;
+import :static_string;
+import :is_whatable_error;
+
+namespace moderna::generic {
+  template <generic::static_string stage, generic::static_string... stages>
+  std::optional<std::string> get_stage_name(size_t id) {
+    if (id == 0) {
+      return stage.as_string();
+    } else if constexpr (sizeof...(stages) == 0) {
+      return std::nullopt;
+    } else {
+      return get_stage_name<stages...>(id);
+    }
+  }
+  export template <generic::static_string... stages> class staged_error : public std::exception {
+    std::string __msg;
+    size_t __stage;
+
+  public:
+    staged_error(std::string msg, size_t stage) : __msg{std::move(msg)}, __stage{stage} {}
+    size_t stage() const noexcept {
+      return __stage;
+    }
+    std::optional<std::string> stage_name() const noexcept {
+      return get_stage_name<stages...>(__stage);
+    }
+    const char *what() const noexcept {
+      return __msg.c_str();
+    }
+    std::string_view as_view() const noexcept {
+      return std::string_view{__msg.c_str()};
+    }
+  };
+  export template <generic::static_string... stages> struct staged_error_creator {
+    using error_type = staged_error<stages...>;
+    size_t stage;
+
+    error_type operator()(const generic::is_whatable_error auto &e) {
+      return error_type{e.what(), stage};
+    }
+  };
+}
