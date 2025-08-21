83c419521dc5280611826d4345c4fb1fd21bb32d
Jonathan Willianto <jowillianto@gmail.com>
2025-08-21 15:04:33 +0900

    [#main] modernize concept requirement

--- src/is_formattable_error.cc
+++ src/is_formattable_error.cc
@@ -6,31 +6,16 @@ export module moderna.generic:is_formattable_error;
 
 namespace moderna::generic {
   export template <class E>
-  concept is_formattable_error =
-    std::is_constructible_v<std::string, decltype(std::declval<E>().what())> ||
-    std::formattable<E, char>;
+  concept is_formattable_error = std::derived_from<E, std::exception> || std::formattable<E, char>;
 
   export struct error_formatter {
     std::string msg;
-    template <is_formattable_error E> error_formatter(const E &e) {
+    template <is_formattable_error E> constexpr error_formatter(const E &e) {
       if constexpr (std::formattable<E, char>) {
         msg = std::format("{}", e);
       } else {
         msg = std::string{e.what()};
       }
-    }
-  };
-
-  export class generic_error : public std::exception {
-    std::unique_ptr<std::exception> __err;
-
-  public:
-    generic_error(std::unique_ptr<std::exception> e) : __err{std::move(e)} {}
-    template <std::derived_from<std::exception> T>
-    generic_error(T err) : __err{std::make_unique<T>(std::move(err))} {}
-
-    const char *what() const noexcept {
-      return __err->what();
     }
   };
 }
